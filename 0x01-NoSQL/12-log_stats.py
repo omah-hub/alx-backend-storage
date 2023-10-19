25978c328265d5dc17de1d6c4c438a87c3759ea4
omah-hub <orjichioma422@yahoo.com>
2023-10-19 22:17:10 +0000

    my commit

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -1,19 +1,30 @@
 #!/usr/bin/env python3
-"""Print nginx logs """
+'''Task 12's module.
+'''
 from pymongo import MongoClient
 
-if __name__ == "__main__":
-    """ checking for all elements in collection """
+
+def print_nginx_request_logs(nginx_collection):
+    '''Prints stats about Nginx request logs.
+    '''
+    print('{} logs'.format(nginx_collection.count_documents({})))
+    print('Methods:')
+    methods = ['GET', 'POST', 'PUT', 'PATCH', 'DELETE']
+    for method in methods:
+        req_count = len(list(nginx_collection.find({'method': method})))
+        print('\tmethod {}: {}'.format(method, req_count))
+    status_checks_count = len(list(
+        nginx_collection.find({'method': 'GET', 'path': '/status'})
+    ))
+    print('{} status check'.format(status_checks_count))
+
+
+def run():
+    '''Provides some stats about Nginx logs stored in MongoDB.
+    '''
     client = MongoClient('mongodb://127.0.0.1:27017')
-    collection = client.logs.nginx
+    print_nginx_request_logs(client.logs.nginx)
 
-    print(f"{collection.estimated_document_count()} logs")
 
-    print("Methods:")
-    for method in ["GET", "POST", "PUT", "PATCH", "DELETE"]:
-        method_count = collection.count_documents({'method': method})
-        print(f"\tmethod {method}: {method_count}")
-
-    check_get = collection.count_documents(
-        {'method': 'GET', 'path': "/status"})
-    print(f"{check_get} status check")
+if __name__ == '__main__':
+    run()
