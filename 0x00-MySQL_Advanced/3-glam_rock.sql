fd7f1d033cfee2fb38eb1c3a7c5a61018bc73c0d
omah-hub <orjichioma422@yahoo.com>
2023-10-19 22:22:11 +0000

    correction made

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,17 +1,6 @@
--- This script ranks all bands by their lifespan that have Glam rock as their primary style.
--- Create temporary table to store the bands' lifespan
-CREATE TEMPORARY TABLE IF NOT EXISTS bands_lifespan (
-    band_name VARCHAR(255),
-    lifespan INT
-);
-
-INSERT INTO bands_lifespan (band_name, lifespan)
-SELECT band_name,
-       EXTRACT(YEAR FROM MAX(split)::DATE) - EXTRACT(YEAR FROM MIN(formed)::DATE) AS lifespan
-FROM metal_bands
-WHERE main_style = 'Glam rock'
-GROUP BY band_name;
-
-SELECT band_name, lifespan
-FROM bands_lifespan
-ORDER BY lifespan DESC;
+-- Lists all bands with Glam rock as their main style, ranked by their longevity.
+-- SELECT band_name, (IFNULL(split, YEAR(CURRENT_DATE())) - formed) AS lifespan
+SELECT band_name, (IFNULL(split, '2020') - formed) AS lifespan
+    FROM metal_bands
+    WHERE FIND_IN_SET('Glam rock', IFNULL(style, "")) > 0
+    ORDER BY lifespan DESC;
